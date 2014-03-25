7274da0d0d87f3949a0b2c87b20a9eff1d13b819
raptureaid <raptureaid@users.noreply.github.com>
2014-03-25 11:35:25 -0400

    Moving expandable.js to the custom folder

--- source/js/custom/expandable.js
+++ source/js/custom/expandable.js
@@ -1,27 +1,66 @@
-/*$(document).ready(function(){
-    alert('testing');
-    var expansions = $('.expandable');
-    expansions.toggleClass('on');
-    
-    expansions.children('header').click(function(){
-        $(this).parent().toggleClass('on');
-    });
-    expansions.children('ul').click(function(){
-        alert('hello');
-        $(this).parent().toggleClass('on');
-    });
-});*/
-
 $(document).ready(function(){
-    alert('hello');
-    var expansions = $('.expandable');
-    expansions.toggleClass('on');
-    
-    expansions.children('header').click(function(){
-        $(this).parent().toggleClass('on');
-    });
-    expansions.children('ul.menu-vertical').click(function() {
-        $(this).parent().toggleClass('on');
+    /*
+        Each usage may have unique rules, so walk through
+        them and provide a generic fallback.
+    */
+    $('.expandable').each(function(index, element) {
+        /*
+            Accordian
+            The expand/collapse feature is only triggered when the
+            ex-handler is clicked. The ex-panel may contain clickable
+            items, so we don't want this to unnecessarily close.
+            Close only if javascript is enabled to not hide content.
+        */
+        //if ( $(element).parent().hasClass('accordion') ) {
+        if ( $(element).parent().hasClass('accordion') ) {
+            // Begin Closed
+            $(element).not('.is-closed').toggleClass('is-closed');
+
+            // Click Event
+            $(element).children('.ex-handle').click(function(e) {
+                e.preventDefault();
+                $(this).parent().toggleClass('is-closed is-open');
+            });
+        }
+
+        /*
+            Block Listing
+            Add extra content to the block listing with the expandable
+            feature. Expansion will slide the 'hidden' content into
+            the blocks' container.
+        */
+
+        else if ( $(element).parent().hasClass('block_listing') ) {
+            // Listen for click
+            $(element).children('.ex-handle').click(function(e) {
+                e.preventDefault();
+                $(this).parents('.block_listing').toggleClass('is-closed is-open');
+            });
+        }
+
+        /*
+            Navigation
+            Close only if javascript is enabled to not hide content.
+        */
+        else if ( $(element).hasClass('nav-main') ) {
+            // Begin closed
+            $(element).not('.is-closed').toggleClass('is-closed');
+
+            // Click Event
+            $(element).click(function(e) {
+                e.preventDefault();
+                $(this).toggleClass('is-closed is-open');
+            });
+        }
+
+        /*
+            Default Expansion
+        */
+        else {
+            $(element).click(function(e) {
+                e.preventDefault();
+                $(this).toggleClass('is-closed is-open');
+            });
+        }
     });
 });
-
