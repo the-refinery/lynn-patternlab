6895ed1e9c4826460e171bea18057d14dcc1296a
raptureaid <raptureaid@users.noreply.github.com>
2014-03-25 11:21:59 -0400

    Adding logic to position pullquotes

--- source/js/custom/pullquote.js
+++ source/js/custom/pullquote.js
@@ -2,9 +2,16 @@ $(function() {
 	$('.pullquote').each(function() {
 		var $parentObject = $(this).parent();
 
+		// Ensure the parent will accept the float
 		$parentObject.css('position', 'relative');
+
+		// Make the pullquote
 		$(this).clone()
 			.addClass('pulledquote')
+			.removeClass('pullquote')
 			.prependTo($parentObject);
+
+		// Remove any positioning from original span
+		$(this).removeClass('center left right');
 	});
 });
