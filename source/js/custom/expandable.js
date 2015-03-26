8288203903a7ee10e428b4cd0dd7a05360447ae9
raptureaid <raptureaid@att.net>
2015-03-26 13:51:43 -0400

    Javascript only class hook

--- source/js/custom/expandable.js
+++ source/js/custom/expandable.js
@@ -1,22 +1,37 @@
 $(document).ready(function(){
     /*
-        Each usage may have unique rules, so walk through
-        them and provide a generic fallback.
-    */
-    $('.expandable').each(function(index, element) {
-        /*
-            Accordian
-            The expand/collapse feature is only triggered when the
-            ex-handler is clicked. The ex-panel may contain clickable
-            items, so we don't want this to unnecessarily close.
-            Close only if javascript is enabled to not hide content.
-        */
-        //if ( $(element).parent().hasClass('accordion') ) {
-        if ( $(element).parent().hasClass('accordion') ) {
-            // Begin Closed
-            $(element).not('.is-closed').toggleClass('is-closed');
+        The expand/collapse feature is only triggered when clicked,
+        either on the element or a specified ex-handler. The 
+        ex-panel may contain clickable items, so we don't want 
+        this to unnecessarily close.
 
-            // Click Event
+        A js-enabled class is inserted as a flag for CSS.
+
+        The default action of the expandable object is blocked.
+        (i.e. <a class="expandable" href="http://google.com"> will 
+        only trigger open/close of the expandable panel and never 
+        go to the URL of the link.) Disable this on a per use basis
+        by removing e.preventDefault() from the click event
+    */
+    $('.js-expand').each(function(index, element) {
+        /*
+            ex-handle, begin closed, no default
+
+            The expandable class is inside a parent container, 
+            which requires the triggers be applied to on the
+            parent.
+
+            Types:
+                Accordian
+                Card (with navigation)
+        */
+        if (
+            $(element).parent().is('.accordion, .card-content, .card-details')
+        ) {
+            // Start state
+            $(element).addClass('js-enabled').not('.is-closed').toggleClass('is-closed');
+
+            // Click event
             $(element).children('.ex-handle').click(function(e) {
                 e.preventDefault();
                 $(this).parent().toggleClass('is-closed is-open');
@@ -24,36 +39,25 @@ $(document).ready(function(){
         }
 
         /*
-            Block Listing
-            Add extra content to the block listing with the expandable
-            feature. Expansion will slide the 'hidden' content into
-            the blocks' container.
-        */
+            No ex-handle, begin closed, allow default
 
-        else if ( $(element).parent().hasClass('block_listing') ) {
-            // Listen for click
-            $(element).children('.ex-handle').click(function(e) {
-                e.preventDefault();
-                $(this).parents('.block_listing').toggleClass('is-closed is-open');
-            });
-        }
-
-        /*
-            Navigation
-            Close only if javascript is enabled to not hide content.
+            Types:
+                Navigation (main)
         */
-        else if ( $(element).hasClass('nav-main') ) {
-            // Begin closed
+        else if (
+            $(element).hasClass('nav-main')
+        ) {
+            // Start state
             $(element).addClass('js-enabled').not('.is-closed').toggleClass('is-closed');
 
-            // Click Event
+            // Click event
             $(element).click(function(e) {
                 $(this).toggleClass('is-closed is-open');
             });
         }
 
         /*
-            Default Expansion
+            Default
         */
         else {
             $(element).click(function(e) {
