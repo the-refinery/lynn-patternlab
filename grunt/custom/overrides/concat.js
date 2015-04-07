d421d3b8effc9bf2edcf2fcf02686db90a7d5dcf
raptureaid <raptureaid@att.net>
2015-04-07 14:28:53 -0400

    Converting tabs to spaces

--- grunt/custom/overrides/concat.js
+++ grunt/custom/overrides/concat.js
@@ -1,15 +1,15 @@
 module.exports = {
-	jquery: {
-		src: '<%= config.js.src %>/lib/jquery-2.0.0b2.js',
-		dest: '<%= config.js.src %>/dist/jquery-2.0.0b2.js'
-	},
+    jquery: {
+        src: '<%= config.js.src %>/lib/jquery-2.0.0b2.js',
+        dest: '<%= config.js.src %>/dist/jquery-2.0.0b2.js'
+    },
 
-	js: {
-		src: [
-			'<%= config.js.src %>/lib/jquery.flexslider.js',
-			'<%= config.js.src %>/lib/featherlight-1.2.0.js',
+    js: {
+        src: [
+            '<%= config.js.src %>/lib/jquery.flexslider.js',
+            '<%= config.js.src %>/lib/featherlight-1.2.0.js',
             '<%= config.js.src %>/lib/fitvids.js',
             '<%= config.js.src %>/custom/**/*.js'
-		],
-	}
+        ],
+    }
 }
