6c7519cb45ef149b93b6351685530a2eec5ec8da
raptureaid <raptureaid@att.net>
2015-04-13 12:53:01 -0400

    Sticky items no longer tied to a specific nav element.

---
+++ source/js/dev/custom/sticky-items.js
@@ -0,0 +1,61 @@
+/**
+ *  Sticky Items
+ *
+ *  Looks for page elements with a class `js-sticky`,
+ *  and toggles a class `fixed` to allow them to 
+ *  stick once they reach the top off the page. 
+**/
+
+$(document).ready(function() {
+    var stickyItems = $('.js-sticky'),
+        origOffsetY =  [];
+
+    // Function to get original position of items
+    function origOffset(items) {
+        items.each(function(index, element) {
+            origOffsetY[index] = $(this).offset().top;
+        });
+    }
+
+    // Function to toggle sticky class
+    function stickEm(items) {
+        // Run once to get initial values
+        if (!origOffsetY[0]) {
+            origOffset(items);  
+        }
+
+        // Loop the sticky items to determine when to stick
+        return items.each(function(index, element) {
+            window.scrollY > origOffsetY[index] ?
+                $(this).addClass('fixed') :
+                $(this).removeClass('fixed');
+        });
+    }
+
+    // Initial state
+    // If pre-scrolled, see if anything sticks
+    stickEm(stickyItems);
+
+    // Scroll should trigger sticky check
+    $(window).scroll(
+        debounce(function(e) {
+            stickEm(stickyItems);
+        }, 10)
+    );
+
+    // Window resize should recalculate positions
+    $(window).resize(
+        debounce(function(e) {
+            // Remove all fixed tags to get true original position
+            stickyItems.each(function(index, element) {
+                $(this).removeClass('fixed');
+            });
+
+            // New original position
+            origOffset(stickyItems);
+
+            // Check if anything should be stuck
+            stickEm(stickyItems);
+        }, 500)
+    );
+});
