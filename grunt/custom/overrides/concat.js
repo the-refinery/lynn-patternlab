5c6fdd27d2d7288071a63968cac6fcd2848e15cd
raptureaid <raptureaid@att.net>
2015-04-13 12:45:53 -0400

    Grunt tasks should watch and act on JS files in a dev folder only

--- grunt/custom/overrides/concat.js
+++ grunt/custom/overrides/concat.js
@@ -1,15 +1,13 @@
 module.exports = {
     jquery: {
-        src: '<%= config.js.src %>/lib/jquery-2.0.0b2.js',
+        src: '<%= config.js.src %>/dev/lib/jquery-2.0.0b2.js',
         dest: '<%= config.js.src %>/dist/jquery-2.0.0b2.js'
     },
 
     js: {
         src: [
-            '<%= config.js.src %>/lib/jquery.flexslider.js',
-            '<%= config.js.src %>/lib/featherlight-1.2.0.js',
-            '<%= config.js.src %>/lib/fitvids.js',
-            '<%= config.js.src %>/custom/**/*.js'
+            '<%= config.js.src %>/dev/addons/**/*.js',
+            '<%= config.js.src %>/dev/custom/**/*.js'
         ],
     }
 }
